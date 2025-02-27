6aa842565b8a7bd2d5dd76aeeabc63ad61d7cb34
andreiz53 <andrei.zamfira53@gmail.com>
2025-02-27 18:40:41 +0200

    add viper and load config

--- cmd/config.go
+++ cmd/config.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // configCmd represents the config command
@@ -15,21 +16,42 @@ var configCmd = &cobra.Command{
 	Short: "Adds a config",
 	Long: `This command helps you set the config for the application.
 The config is used in reading the websites that need to be scraped as well as different configuration settings.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("config called")
-	},
-}
+	Run: setAppConfig}
 
 func init() {
 	rootCmd.AddCommand(configCmd)
 
-	// Here you will define your flags and configuration settings.
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
+type AppConfig struct {
+	Websites []string `mapstructure:"websites"`
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func LoadAppConfig(path string) (*AppConfig, error) {
+	viper.SetConfigFile(path)
+	viper.SetConfigType("json")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	var config AppConfig
+	err = viper.Unmarshal(&config)
+	return &config, err
+}
+
+func setAppConfig(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		cmd.PrintErr(`The config command accepts one argument, which is the path to the config file. 
+
+Example: cli-go-scraper config ./config.json
+`)
+		return
+	}
+	config, err := LoadAppConfig(args[0])
+	if err != nil {
+		cmd.PrintErr(`Could not load config at path `, args[0], "\n")
+		return
+	}
+	fmt.Println("got config", config)
 }
